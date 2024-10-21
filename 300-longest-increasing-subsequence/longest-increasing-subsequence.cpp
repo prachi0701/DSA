a89139d7c80a07afd3f4437d65bf76be76c49129
Prachi Bhati <112480424+prachi0701@users.noreply.github.com>
2024-10-21 12:13:04 +0530

    Time: 290 ms (29.21%) | Memory: 294.9 MB (27.25%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -14,15 +14,35 @@ public:
             include = 1 + solve(nums, index+1, index, dp);
         }
         int exclude = solve(nums, index+1, prev, dp);
-        
+
         dp[index][prev+1] = max(exclude, include);
         return dp[index][prev+1];
     }
 
-    int lengthOfLIS(vector<int>& nums) {
+    int solveTab(vector<int>& nums){
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        int ans = solve(nums, 0, -1, dp);
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+
+        for(int index = n-1; index>=0; index--){
+            for(int prev = index-1; prev >= -1; prev--){
+                int exclude = dp[index+1][prev+1];
+                int include = 0;
+                if(prev == -1 || nums[index] > nums[prev]){
+                    include = 1 + dp[index+1][index+1];
+                }
+                dp[index][prev+1] = max(include, exclude);
+            }
+        }
+        return dp[0][0];
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        // int n = nums.size();
+        // vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        // int ans = solve(nums, 0, -1, dp);
+        // return ans;
+
+        int ans = solveTab(nums);
         return ans;
     }
 };
