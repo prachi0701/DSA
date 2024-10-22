ba3967f17d8377323b03b5c36280249bc7eb1f16
Prachi Bhati <112480424+prachi0701@users.noreply.github.com>
2024-10-22 13:07:21 +0530

    Time: 11 ms (69.75%) | Memory: 25.6 MB (6.37%) - LeetSync

--- 1489-pizza-with-3n-slices/pizza-with-3n-slices.cpp
+++ 1489-pizza-with-3n-slices/pizza-with-3n-slices.cpp
@@ -25,13 +25,41 @@ public:
         return dp[index][n];
     }
 
-    int maxSizeSlices(vector<int>& slices) {
+    int solveTab(vector<int>& slices){
         int k = slices.size();
-        vector<vector<int>> dp1(k, vector<int>(k/3+1, -1));
-        vector<vector<int>> dp2(k, vector<int>(k/3+1, -1));
-        int case1 = solveMem(0, k-2, slices, k/3, dp1);
-        int case2 = solveMem(1, k-1, slices, k/3, dp2);
-        int ans = max(case1, case2);
-        return ans;
+        vector<vector<int>> dp1(k+2, vector<int>(k+2, 0));
+        vector<vector<int>> dp2(k+2, vector<int>(k+2, 0));
+
+        for(int index = k-2; index >= 0; index--){
+            for(int n = 1; n <= k/3; n++){
+                int include = slices[index] + dp1[index+2][n-1];
+                int exclude = dp1[index+1][n];
+                dp1[index][n] = max(include, exclude);
+            }
+        }
+        int case1 = dp1[0][k/3];
+
+        for(int index = k-1; index >= 1; index--){
+            for(int n = 1; n <= k/3; n++){
+                int include = slices[index] + dp2[index+2][n-1];
+                int exclude = dp2[index+1][n];
+                dp2[index][n] = max(include, exclude);
+            }
+        }
+        int case2 = dp2[1][k/3];
+
+        return max(case1, case2);
+    }
+
+    int maxSizeSlices(vector<int>& slices) {
+        // int k = slices.size();
+        // vector<vector<int>> dp1(k, vector<int>(k/3+1, -1));
+        // vector<vector<int>> dp2(k, vector<int>(k/3+1, -1));
+        // int case1 = solveMem(0, k-2, slices, k/3, dp1);
+        // int case2 = solveMem(1, k-1, slices, k/3, dp2);
+        // int ans = max(case1, case2);
+        // return ans;
+
+        return solveTab(slices);
     }
 };
