156c433b1316ac64edb6454d07d0052917aa8f05
Prachi Bhati <112480424+prachi0701@users.noreply.github.com>
2024-10-23 23:37:59 +0530

    Time: 13 ms (69.48%) | Memory: 9.7 MB (77.28%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -28,9 +28,29 @@ public:
         return dp[n][target];
     }
 
+    int solveTab(int n, int k, int target){
+        vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
+        dp[0][0] = 1;
+
+        for(int dice=1; dice<=n; dice++){
+            for(int t=1; t<=target; t++){
+                int ans = 0;
+                for(int i=1; i<=k; i++){
+                    if(t-i >= 0){
+                        ans = (ans + dp[dice-1][t-i])%MOD;
+                    }
+                }
+                dp[dice][t] = ans;
+            }
+        }
+        return dp[n][target];
+    }
+
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> dp(n+1, vector<int>(target+1, -1));
-        int ans = solve(n, k, target, dp);
-        return ans;
+        // vector<vector<int>> dp(n+1, vector<int>(target+1, -1));
+        // int ans = solve(n, k, target, dp);
+        // return ans;
+
+        return solveTab(n, k, target);
     }
 };
