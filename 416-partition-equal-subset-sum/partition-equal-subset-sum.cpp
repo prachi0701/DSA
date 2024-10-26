c410f7218506ee24a3ebcab5af4f30f2babab950
Prachi Bhati <112480424+prachi0701@users.noreply.github.com>
2024-10-26 17:29:16 +0530

    Time: 86 ms (82.03%) | Memory: 14.1 MB (83.28%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -18,6 +18,48 @@ public:
         return dp[index][target];
     }
 
+
+    int solveTab(vector<int>& nums, int target){
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
+        for(int i=0; i<=n; i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i=n-1; i>=0; i--){
+            for(int j=1; j<=target; j++){
+                int exclude = dp[i+1][j];
+                int include = 0;
+                if(j-nums[i] >= 0){
+                    include = dp[i+1][j-nums[i]];
+                }
+                dp[i][j] = exclude || include;
+            }
+        }
+        return dp[0][target];
+    }
+
+    int solveSpaceOpt(vector<int>& nums, int target){
+        int n = nums.size();
+        vector<int> curr(target+1, 0);
+        vector<int> next(target+1, 0);
+        next[0] = 1;
+
+        for(int i=n-1; i>=0; i--){
+            for(int j=1; j<=target; j++){
+                int exclude = next[j];
+                int include = 0;
+                if(j-nums[i] >= 0){
+                    include = next[j-nums[i]];
+                }
+                curr[j] = exclude || include;
+            }
+            next = curr;
+        }
+        return curr[target];
+    }
+
+
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         int n = nums.size();
@@ -25,12 +67,18 @@ public:
             sum += nums[i];
         }
         int target = sum/2;
-        vector<vector<int>> dp(n+1, vector<int>(target+1, -1));
+
+        // vector<vector<int>> dp(n+1, vector<int>(target+1, -1));
 
         if(sum%2 != 0){
             return false;
         }else{
-            int ans = solve(nums, target, 0, dp);
+            // int ans = solve(nums, target, 0, dp);
+
+            // int ans = solveTab(nums, target);
+
+            int ans = solveSpaceOpt(nums, target);
+
             if(ans == 1){
                 return true;
             }else{
