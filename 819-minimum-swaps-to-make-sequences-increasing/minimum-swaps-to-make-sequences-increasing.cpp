3449d9f5fd59879905e56c91c332f29de09ca8b3
Prachi Bhati <112480424+prachi0701@users.noreply.github.com>
2024-10-26 19:01:21 +0530

    Time: 75 ms (95.29%) | Memory: 123.4 MB (45.40%) - LeetSync

--- 819-minimum-swaps-to-make-sequences-increasing/minimum-swaps-to-make-sequences-increasing.cpp
+++ 819-minimum-swaps-to-make-sequences-increasing/minimum-swaps-to-make-sequences-increasing.cpp
@@ -30,13 +30,43 @@ public:
         return dp[index][swapped];
     }
 
-    int minSwap(vector<int>& nums1, vector<int>& nums2) {
+    int solveTab(vector<int>& nums1, vector<int>& nums2){
         nums1.insert(nums1.begin(), -1);
         nums2.insert(nums2.begin(), -1);
-        bool swapped = 0;
         int n = nums1.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
 
-        return solve(nums1, nums2, 1, swapped, dp);
+        for(int index = n-1; index>=1; index--){
+            for(int swapped=1; swapped >=0; swapped--){
+                int prev1 = nums1[index-1];
+                int prev2 = nums2[index-1];
+                if(swapped){
+                    swap(prev1, prev2);
+                }
+                int ans = INT_MAX;
+
+                // already increasing NO SWAP
+                if(nums1[index] > prev1 && nums2[index] > prev2){
+                    ans = dp[index+1][0];
+                }
+                if(nums1[index] > prev2 && nums2[index] > prev1){
+                    ans = min(ans, 1 + dp[index+1][1]);
+                }
+                dp[index][swapped] = ans;
+            }
+        }
+        return dp[1][0];
+    }
+
+    int minSwap(vector<int>& nums1, vector<int>& nums2) {
+        // nums1.insert(nums1.begin(), -1);
+        // nums2.insert(nums2.begin(), -1);
+        // bool swapped = 0;
+        // int n = nums1.size();
+        // vector<vector<int>> dp(n, vector<int>(2, -1));
+
+        // return solve(nums1, nums2, 1, swapped, dp);
+
+        return solveTab(nums1, nums2);
     }
 };
