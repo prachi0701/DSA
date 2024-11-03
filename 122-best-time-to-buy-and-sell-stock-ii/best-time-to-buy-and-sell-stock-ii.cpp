d73b116d0da812d2e03fe8dffc3cef023760f4d5
Prachi Bhati <112480424+prachi0701@users.noreply.github.com>
2024-11-03 19:31:41 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (72.83%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -64,6 +64,30 @@ public:
         return dp[0][1];
     }
 
+    int solveSO(vector<int>& prices){
+        int n = prices.size();
+        vector<int> next(2, 0);
+        vector<int> curr(2, 0);
+
+        for(int index = n-1; index>=0; index--){
+            for(int buy = 0; buy<=1; buy++){
+                int profit = 0;
+                if(buy == 1){
+                    int include = (-1)*prices[index] + next[0];
+                    int exclude = 0 + next[1];
+                    profit = max(include, exclude);
+                }else{
+                    int include = prices[index] + next[1];
+                    int exclude = 0 + next[0];
+                    profit = max(include, exclude);
+                }
+                curr[buy] = profit;
+            }
+            next = curr;
+        }
+        return curr[1];
+    }
+
 
     int maxProfit(vector<int>& prices) {
         // return solve(0, 1, prices);
@@ -72,6 +96,8 @@ public:
         // vector<vector<int>> dp(n, vector<int>(2, -1));
         // return solveMem(0, 1, prices, dp);
 
-        return solveTab(prices);
+        // return solveTab(prices);
+
+        return solveSO(prices);
     }
 };
