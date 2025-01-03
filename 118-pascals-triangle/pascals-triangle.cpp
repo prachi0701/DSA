61f4adb05e3ab6e02b09d491b59ab29b3b901ac3
Prachi Bhati <112480424+prachi0701@users.noreply.github.com>
2025-01-03 17:12:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (9.25%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,30 +1,49 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
+        // vector<vector<int>> ans;
+        // vector<int> v;
+        // v.push_back(1);
+        // ans.push_back(v);
+        // if(numRows == 1){
+        //     return ans;
+        // }
+        // v.push_back(1);
+        // ans.push_back(v);
+        // if(numRows == 2){
+        //     return ans;
+        // }
+        
+        // for(int i=2; i<numRows; i++){
+        //     vector<int> temp;
+        //     temp.push_back(1);
+        //     int l = 0;
+        //     int m = 1;
+        //     while(m < ans[i-1].size()){
+        //         temp.push_back(ans[i-1][l] + ans[i-1][m]);
+        //         l++;
+        //         m++;
+        //     }
+        //     temp.push_back(1);
+        //     ans.push_back(temp);
+        // }
+        // return ans;
+
+
         vector<vector<int>> ans;
         vector<int> v;
         v.push_back(1);
         ans.push_back(v);
-        if(numRows == 1){
-            return ans;
-        }
-        v.push_back(1);
-        ans.push_back(v);
-        if(numRows == 2){
-            return ans;
-        }
-        
-        for(int i=2; i<numRows; i++){
+
+        for(int row=2; row<=numRows; row++){
             vector<int> temp;
             temp.push_back(1);
-            int l = 0;
-            int m = 1;
-            while(m < ans[i-1].size()){
-                temp.push_back(ans[i-1][l] + ans[i-1][m]);
-                l++;
-                m++;
+            int element = 1;
+            for(int col=1; col<row; col++){
+                element = element * (row-col);
+                element = element / col;
+                temp.push_back(element);
             }
-            temp.push_back(1);
             ans.push_back(temp);
         }
         return ans;
