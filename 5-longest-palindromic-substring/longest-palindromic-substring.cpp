c8e2ad5d0a410882a1909e1e5ca9e9d4ae80536d
Prachi Bhati <112480424+prachi0701@users.noreply.github.com>
2025-01-12 22:08:32 +0530

    Time: 9 ms (77.19%) | Memory: 9.4 MB (74.03%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,48 +1,28 @@
 class Solution {
 public:
-
-    bool check(string s, int i, int j){
-        while(i < j){
-            if(s[i] != s[j]){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-    }
-
     string longestPalindrome(string s) {
         int n = s.length();
-    if (n == 0) return "";
+        int maxi = 1;
+        int start = 0;
 
-    int start = 0, maxLen = 1;
+        for(int i=0; i<n; i++){
 
-    // Traverse the input string
-    for (int i = 0; i < n; i++) {
+            for(int j=0; j<=1; j++){
+                int low = i;
+                int high = i+j;
 
-        // THIS RUNS TWO TIMES 
-        // for both odd and even length
-        // palindromes. j = 0 means odd
-        // and j = 1 means odd length
-        for (int j = 0; j <= 1; j++) {
-            int low = i;
-            int hi = i + j; 
-
-            // Expand substring while it is a palindrome
-            // and in bounds
-            while (low >= 0 && hi < n && s[low] == s[hi]) {
-                int currLen = hi - low + 1;
-                if (currLen > maxLen) {
-                    start = low;
-                    maxLen = currLen;
+                while(low>=0 && high <n && s[low] == s[high]){
+                    if(high-low +1 > maxi){
+                        start = low;
+                        maxi = high-low +1;
+                    }
+                    low--;
+                    high++;
                 }
-                low--;
-                hi++;
             }
         }
-    }
 
-    return s.substr(start, maxLen);
+        string ans = s.substr(start, maxi);
+        return ans;
     }
 };
