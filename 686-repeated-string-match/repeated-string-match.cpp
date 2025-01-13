229a998a11f1e577afb5e25f2af5c075eecb7a5b
Prachi Bhati <112480424+prachi0701@users.noreply.github.com>
2025-01-13 22:29:21 +0530

    Time: 2286 ms (5.07%) | Memory: 10.8 MB (28.26%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+
+    bool isSubstring(string a, string b){
+        for(int i=0; i<a.length(); i++){
+            int j;
+            for(j=0; j<b.length(); j++){
+                if(a[i+j] != b[j]){
+                    break;
+                }
+            }
+            if(j == b.length()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int repeatedStringMatch(string a, string b) {\
+        int n = (b.length() + a.length() - 1) / a.length();
+
+        // 4 possible way
+        // x n*a x ----- x n*a ------- n*a x ----- n*a
+
+        string temp = a;
+        int cnt = 1;
+
+        while(cnt != n){
+            a += temp;
+            cnt++; 
+        }
+
+        if(isSubstring(a, b) == true){
+            return n;
+        }
+
+        a += temp;
+        if(isSubstring(a, b) == true){
+            return n+1;
+        }
+
+        a += temp;
+        if(isSubstring(a, b) == true){
+            return n+2;
+        }
+
+        return -1;
+    }
+};
