58a10358fb2ebcd44c39a7567d24244fdbf66565
Prachi Bhati <112480424+prachi0701@users.noreply.github.com>
2025-05-03 18:00:51 +0530

    Time: 14 ms (14.09%) | Memory: 20 MB (10.82%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,53 +1,24 @@
 class Solution {
 public:
 
-    bool isSafe(int col, int row, vector<string> temp, int n){
-        int dumCol = col;
-        int dumRow = row;
 
-        // for upper diagnol
-        while(dumRow >= 0 && dumCol >= 0){
-            if(temp[dumRow][dumCol] == 'Q'){
-                return false;
-            }
-            dumRow--;
-            dumCol--;
-        }
-
-        // for left side
-        dumRow = row;
-        dumCol = col;
-        while(dumCol >= 0){
-            if(temp[dumRow][dumCol] == 'Q'){
-                return false;
-            }
-            dumCol--;
-        }
-
-        // for lower diagonal
-        dumRow = row;
-        dumCol = col;
-        while(dumCol >= 0 && dumRow < n){
-            if(temp[dumRow][dumCol] == 'Q'){
-                return false;
-            }
-            dumCol--;
-            dumRow++;
-        }
-        return true;
-    }
-
-    void solve(int col, vector<vector<string>>& ans, vector<string> temp, int n){
+    void solve(int col, vector<vector<string>>& ans, vector<string> temp, int n, vector<int> leftRow, vector<int> lowerDiagonal, vector<int> upperDiagonal){
         if(col == n){
             ans.push_back(temp);
             return;
         }
 
         for(int row=0; row<n; row++){
-            if(isSafe(col, row, temp, n)){
+            if(leftRow[row] == 0 && lowerDiagonal[row+col] == 0 && upperDiagonal[n-1 + col - row] == 0){
                 temp[row][col] = 'Q';
-                solve(col+1, ans, temp, n);
+                leftRow[row] = 1;
+                lowerDiagonal[row+col] = 1;
+                upperDiagonal[n-1 + col - row] = 1;
+                solve(col+1, ans, temp, n, leftRow, lowerDiagonal, upperDiagonal);
                 temp[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiagonal[row+col] = 0;
+                upperDiagonal[n-1 + col - row] = 0;
             }
         }
     }
@@ -59,7 +30,10 @@ public:
         for(int i=0; i<n; i++){
             temp[i] = s;
         }
-        solve(0, ans, temp, n);
+        vector<int> leftRow(n, 0);
+        vector<int> lowerDiagonal(2*n-1, 0);
+        vector<int> upperDiagonal(2*n-1, 0);
+        solve(0, ans, temp, n, leftRow, lowerDiagonal, upperDiagonal);
         return ans;
     }
 };
