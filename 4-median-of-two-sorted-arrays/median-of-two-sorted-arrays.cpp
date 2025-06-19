d0c6ffbf64a3c02172f32d686c53c01a6156eb11
Prachi Bhati <112480424+prachi0701@users.noreply.github.com>
2025-06-19 17:21:48 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (79.31%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,50 +1,38 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int i = 0;
-        int j = 0;
-        int cnt = 0;
-        int med1 = -1;
-        int med2 = -1;
-        int n = nums1.size() + nums2.size();
-        int ind1 = n/2;
-        int ind2 = n/2 - 1;
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        if(n2 < n1){
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int left = (n1 + n2 + 1)/2;
+        int low = 0;
+        int high = n1;
+        int n = n1 + n2;
 
-        while(i < nums1.size() && j < nums2.size()){
-            if(nums1[i] < nums2[j]){
-                if(cnt == ind1) med1 = nums1[i];
-                if(cnt == ind2) med2 = nums1[i];
-                i++;
-                cnt++;
-            }else{
-                if(cnt == ind1) med1 = nums2[j];
-                if(cnt == ind2) med2 = nums2[j];
-                j++;
-                cnt++;
+        while(low <= high){
+            int mid1 = (low + high)/2;
+            int mid2 = left - mid1;
+            int l1 = INT_MIN;
+            int l2 = INT_MIN;
+            int r1 = INT_MAX;
+            int r2 = INT_MAX;
+            if(mid1-1 >= 0) l1 = nums1[mid1-1];
+            if(mid2-1 >= 0) l2 = nums2[mid2-1];
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2];
+            if(l1 <= r2 && l2 <= r1){
+                if(n % 2 == 1) return max(l1, l2);
+                return ((double)(max(l1, l2) + min(r1, r2)))/2.0;
+            }
+            else if(l1 > r2){
+                high = mid1-1;
+            }
+            else{
+                low = mid1+1;
             }
         }
-
-        while(i < nums1.size()){
-            if(cnt == ind1) med1 = nums1[i];
-            if(cnt == ind2) med2 = nums1[i];
-            i++;
-            cnt++;
-        }
-        while(j < nums2.size()){
-            if(cnt == ind1) med1 = nums2[j];
-            if(cnt == ind2) med2 = nums2[j];
-            j++;
-            cnt++;
-        }
-
-        double ans = 0;
-        int mid = n/2;
-        if(n%2 == 0){
-            ans = (med1 + med2*(1.0))/2;
-        }
-        else{
-            ans = med1*(1.0);
-        }
-        return ans;
+        return 0;
     }
 };
